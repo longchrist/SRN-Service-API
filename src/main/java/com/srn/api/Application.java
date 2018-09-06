9299039b6e160d667fc83f27db2cf60a76613025
vikraa <vikra.alviano@yahoo.com>
2018-09-07 00:11:42 +0700

    integrate with hikaricpconfig hardcoded configuration

--- src/main/java/com/srn/api/Application.java
+++ src/main/java/com/srn/api/Application.java
@@ -2,6 +2,10 @@ package com.srn.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import javax.sql.DataSource;
+
 
 /**
  * @author long
@@ -13,5 +17,10 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public DataSource hikariCPConfig() {
+		return new HikariCPConfig().datasource();
 	}
 }
