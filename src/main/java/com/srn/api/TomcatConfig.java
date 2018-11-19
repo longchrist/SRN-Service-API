45d71565b27b849899f326b244f2e3a1d8a72d33
vikraa <vikra.alviano@yahoo.com>
2018-11-20 03:07:56 +0700

    set tomcat config using properties

--- src/main/java/com/srn/api/TomcatConfig.java
+++ src/main/java/com/srn/api/TomcatConfig.java
@@ -1,5 +1,6 @@
 package com.srn.api;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
@@ -10,9 +11,16 @@ import java.nio.charset.Charset;
 @Component
 public class TomcatConfig  implements EmbeddedServletContainerCustomizer {
 
+    @Value("${app.tomcat.port}")
+    int port;
+
+    @Value("${app.tomcat.uri.encoding}")
+    String encoding;
+
     @Override
     public void customize(ConfigurableEmbeddedServletContainer container) {
         TomcatEmbeddedServletContainerFactory tomcat = (TomcatEmbeddedServletContainerFactory)container;
-        tomcat.setUriEncoding(Charset.forName("UTF-8"));
+        tomcat.setUriEncoding(Charset.forName(encoding));
+        tomcat.setPort(port);
     }
 }
