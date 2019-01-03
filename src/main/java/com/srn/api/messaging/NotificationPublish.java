11895ee2914f1a5971757e2a720f58fb66d41990
vikraa <vikra.alviano@yahoo.com>
2019-01-04 01:16:36 +0700

    remove import serialization

--- src/main/java/com/srn/api/messaging/NotificationPublish.java
+++ src/main/java/com/srn/api/messaging/NotificationPublish.java
@@ -8,7 +8,6 @@ import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.api.client.util.ArrayMap;
-import com.sun.xml.internal.ws.developer.Serialization;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
